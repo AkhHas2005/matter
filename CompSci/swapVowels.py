3620317cceb9ab1fb67004a3ba7558cfdee41322
AkhHas2005 <147824419+AkhHas2005@users.noreply.github.com>
2024-02-26 10:12:04 +0000

    Update swapVowels.py

--- CompSci/swapVowels.py
+++ CompSci/swapVowels.py
@@ -1,13 +1,14 @@
-vowels = ['a', 'e', 'i', 'o', 'u']
-
-def swapVowels(vowels, string):
-	s_vowels = []
-	for char in string:
-		if char in vowels:
-			s_vowels.append(char)
-	vowelsLength = len(vowels)
-	halfVL = vowelsLength // 2
-	for num in range(0, halfVL):
-		thisVowel = vowels[num]
-		thatVowel = vowels[vowelsLength-num]
-		string = string.replace(thisVowel, thatVowel)
+class swapVowels:
+	vowels = ['a', 'e', 'i', 'o', 'u']
+	
+	def swapVowels(vowels, string):
+		s_vowels = []
+		for char in string:
+			if char in vowels:
+				s_vowels.append(char)
+		vowelsLength = len(vowels)
+		halfVL = vowelsLength // 2
+		for num in range(0, halfVL):
+			thisVowel = vowels[num]
+			thatVowel = vowels[vowelsLength-num]
+			string = string.replace(thisVowel, thatVowel)
