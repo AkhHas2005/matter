0e4e391a93846af60d311c5f17dc7697c08f52c7
AkhHas2005 <147824419+AkhHas2005@users.noreply.github.com>
2024-03-02 13:28:12 +0000

    Update and rename WhatsApp message.py to CompSci/WhatsAppMessageSender

--- WhatsApp message.py
+++
@@ -1,8 +0,0 @@
-import pywhatkit
-
-contact = input('Enter a contact number:  ')
-message = input('Enter a message to send to',contact,': ')
-hours = int(input('Enter time to send message(hours): '))
-minutes = int(input('Enter time to send message(minutes): '))
-
-pywhatkit.sendwhatmsg(contact, message, hours, minutes)
