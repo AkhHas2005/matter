134e20ddb5e5432c7610367f9f68bca480424a16
AkhHas2005 <147824419+AkhHas2005@users.noreply.github.com>
2024-04-01 19:13:39 +0100

    Update leetcodeSolutionSearcher.py

--- CompSci/leetcodeSolutionSearcher.py
+++ CompSci/leetcodeSolutionSearcher.py
@@ -1,5 +1,27 @@
 #Tool to search for leetcode solutions 
 solutionMapping = {2784: "isGood",
-								    2444: "countSubarrays",
-									992: "subarraysWithKDistinct"}
-									
+				   2444: "countSubarrays",
+				   992: "subarraysWithKDistinct",
+				   101: "isSymmetric"}
+
+solutionsFile = open("leetcodeSolutions.py")
+solutionsBank = solutionsFile.readlines()
+
+solutionNum = int(input("Enter the number of the problem you want to find the solution of: "))
+solution = ""
+solutionFound = False
+
+if solutionNum in solutionMapping.keys():
+	solutionName = "def "+solutionMapping[solutionNum]
+	for line in solutionsBank:
+		if solutionName in line:
+			solutionFound = True
+		if solutionFound:
+			while "return" not in line:
+				solution += line
+	print("\nThe solution is as follows:\n")
+	print(solution)
+else:
+	print("\nThe solution could not be found")
+
+
