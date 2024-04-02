53e773afd2a49fc7b7bd636a77d785eb7416844a
AkhHas2005 <147824419+AkhHas2005@users.noreply.github.com>
2024-04-02 14:26:21 +0100

    Update leetcodeSolutions.py

--- CompSci/leetcodeSolutions.py
+++ CompSci/leetcodeSolutions.py
@@ -83,3 +83,22 @@ class Solutions:
             if num % digit == 0:
                 digitsCount += 1
         return digitsCount
+
+     def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+        envelopes.sort(key=lambda x: (x[0], -x[1]))
+        LIS = []
+        size = 0
+        for (w, h) in envelopes:
+            if not LIS or h > LIS[-1]:
+                LIS.append(h)
+                size += 1
+            else:
+                l, r = 0, size
+                while l < r:
+                    m = l + (r - l) // 2
+                    if LIS[m] < h:
+                        l = m + 1
+                    else:
+                        r = m
+                LIS[l] = h
+        return size
