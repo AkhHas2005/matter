8e52955d983c9e981bfa99c69422a3ede6a76960
AkhHas2005 <147824419+AkhHas2005@users.noreply.github.com>
2024-04-03 18:59:39 +0100

    Update leetcodeSolutions.py

--- CompSci/leetcodeSolutions.py
+++ CompSci/leetcodeSolutions.py
@@ -102,3 +102,15 @@ class Solutions:
                         r = m
                 LIS[l] = h
         return size
+
+    def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
+        ans = []
+        prefix = [0, arr[0]]
+
+        for i in range(1, len(arr)): 
+            prefix.append(prefix[i] ^ arr[i])
+
+        for left, right in queries: 
+            ans.append(prefix[left] ^ prefix[right + 1])
+
+        return ans
